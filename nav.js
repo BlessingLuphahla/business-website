ea94def6a6fa8a7a87dc480d788ceaa4778e965e
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2024-08-09 00:36:21 +0200

    nav update

--- nav.js
+++ nav.js
@@ -3,14 +3,15 @@ var isOpen = false;
 const open = document.querySelector("img.open-menu");
 const menu = document.querySelector("div.hamburger-menu");
 
-
 open.addEventListener("click", function () {
-  console.log('clicked')
+  console.log("clicked");
   if (!isOpen) {
     menu.style.display = "flex";
     isOpen = true;
+    open.src = "images/close-burger.svg";
   } else {
     menu.style.display = "none";
     isOpen = false;
+    open.src = "images/ham-burger.svg";
   }
 });
