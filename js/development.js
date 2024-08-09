87086a75c83a5ad7bc4aafb5eff32c9f085c3f42
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2024-08-09 17:42:06 +0200

    get a quote for others

---
+++ js/development.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const getAQuote = document.querySelectorAll("a.get-a-quote");
+  
+    getAQuote.forEach((button) => {
+      button.addEventListener("click", function () {
+        const subject = "get a quote";
+        const body = 'Can i have a quote for web development';
+  
+        window.location.href = `mailto:luphahlablessingthamsanqa@gmail.com?subject=${encodeURIComponent(
+          subject
+        )}&body=${encodeURIComponent(body)}`;
+      });
+    });
+  });
+  
