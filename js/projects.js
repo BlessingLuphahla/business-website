353eca2bcd2eaa6746733edf47f6ddbad168cf8f
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-03-26 00:04:42 +0200

    projects update

--- js/projects.js
+++ js/projects.js
@@ -2,5 +2,5 @@ const projects = document.querySelectorAll(".project");
 const projectDescriptions = document.querySelectorAll(".projectDesc");
 
 projectDescriptions.forEach((el) => {
-  el.textContent = el.textContent.split(" ").slice(0, 15).join(" ") + "...";
+  el.textContent = el.textContent.split(" ").slice(0, 25).join(" ") + "...";
 });
