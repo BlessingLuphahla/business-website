0050ec099592e78ccd0d06aa5b1e89b771547786
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-03-26 16:10:23 +0200

    projects update

--- js/projects.js
+++ js/projects.js
@@ -1,6 +1,58 @@
-const projects = document.querySelectorAll(".project");
-const projectDescriptions = document.querySelectorAll(".projectDesc");
+document.addEventListener("DOMContentLoaded", function () {
+  const projectsPreviews = document.querySelectorAll(".project");
+  const projectDescriptions = document.querySelectorAll(".projectDesc");
+  const mainSection = document.querySelector(".main-section");
 
-projectDescriptions.forEach((el) => {
-  el.textContent = el.textContent.split(" ").slice(0, 25).join(" ") + "...";
+  const projectPreview = document.createElement("div");
+  projectPreview.classList.add("project-preview");
+
+  var isOpen = false;
+
+  projectsPreviews.forEach((el) => {
+    el.addEventListener("click", function () {
+      mainSection.appendChild(projectPreview);
+
+      const image = el.firstElementChild.getAttribute("src");
+      console.log(image);
+
+      const title =
+        el.firstElementChild.nextElementSibling.firstElementChild.textContent;
+      const desc =
+        el.firstElementChild.nextElementSibling.lastElementChild.textContent;
+      const hiddenTextElement = el.querySelector(".hiddenText");
+
+      const link = el.lastElementChild.getAttribute("href");
+
+      if (!isOpen) {
+        projectPreview.style.display = "flex";
+        projectPreview.innerHTML = `
+        <div class="previewWrapper scale-up-center">
+           <img src='${image}' alt="previewImage" class="previewImage">
+           <div class="previewText">
+            <div class="close">×</div>
+              <h2 class="previewProjectTitle">${title}</h2>
+              <div class="previewNote">Please Note That Due To Our <a class="previewNoteLink" href="privacy policy.html">Privacy Policy</a> We Can Not display actual projects of clients, just a similar project with core features</div>
+            <div class="previewDesc">${
+              desc + hiddenTextElement.textContent
+            }</div>
+            <a href=${link}>Visit Project</a></div>
+        </div>
+        `;
+
+        isOpen = true;
+      } else {
+        projectPreview.style.display = "none";
+        isOpen = false;
+      }
+
+      const close = document.querySelector(".previewWrapper .close");
+
+      close.addEventListener("click", function () {
+        isOpen = false;
+        projectPreview.style.display = "none";
+        this.removeEventListener("click");
+      });
+    });
+  });
+  console.log(description);
 });
